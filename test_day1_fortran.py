712d593a8fb8bddfbf89e3c4a0c8617145c05b85
bryceshirley <bryce.shirley@stfc.ac.uk>
2025-12-02 16:32:46 +0000

    Add pytests for dial and environmt.yml

--- test_day1_fortran.py
+++ test_day1_fortran.py
@@ -1,13 +1,14 @@
 """Unit tests for Fortran-compiled day1 module functions."""
-from dbm import gnu
-import os
 import subprocess
-
-import pytest
-import numpy as np
 import time
+import os
+
+import numpy as np
+import pytest
 
 import day1_fortran.dial_utils_fortran as duf
+# import day1_fortran.part2_main_fortran as p2f
+# import day1_fortran.part1_main_fortran as p1f
 
 
 @pytest.mark.parametrize(
@@ -19,6 +20,9 @@ import day1_fortran.dial_utils_fortran as duf
     ],
 )
 def test_turn_right(current_position, turn_amount, expected_position):
+    """
+    Test turn_right function with various cases.
+    """
     pos = np.array(current_position)
     duf.dial_utils.turn_dial(pos, turn_amount, 'R')
     assert pos == expected_position
@@ -38,6 +42,49 @@ def test_turn_left(current_position, turn_amount, expected_position):
     pos = np.array(current_position)
     duf.dial_utils.turn_dial(pos, turn_amount, 'L')
     assert pos == expected_position
+
+def test_read_input():
+    """
+    Test read_input function to ensure it reads lines correctly from a file.
+    """
+    test_lines = ["R10", "L20", "R30"]
+    test_file = "test_input.txt"
+    with open(test_file, "w") as f:
+        f.write("\n".join(test_lines))
+
+    max_lines = 20000
+    turn_list = duf.dial_utils.read_input(test_file, max_lines)
+
+    # Convert Fortran array of bytes to Python list of strings
+    arr, val = turn_list
+    py_list = [x.decode().strip() for x in arr if x.strip()]
+
+    # delete the test file after reading
+    os.remove(test_file)
+
+    assert py_list == test_lines
+
+
+@pytest.mark.parametrize(
+    "current_position, turn_amount, direction, expected_lands",
+    [
+        (50, 68, 'L', 1),
+        (50, 25, 'R', 0),
+        (50, 1000, 'R', 10),
+        (50, 1000, 'L', 10),
+        (50, 150, 'R', 2),
+        (50, 150, 'L', 2),
+        (0, 23, 'L', 0),
+        (23, 23, 'L', 1),
+        (95, 5, 'R', 1),
+    ],
+)
+def test_turn_dial(current_position, turn_amount, direction, expected_lands):
+    """
+    Test turn_dial function for both directions and various turn amounts.
+    """
+    assert duf.dial_utils.turn_dial(current_position, turn_amount,
+                     direction) == expected_lands
 
 def test_integration_part1():
     """
